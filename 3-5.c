6b11bfce85bac00dd34805749d93d34a5d080405
yungun12 <162670150+yungun12@users.noreply.github.com>
2024-06-25 14:45:45 +0900

    printf() 함수와 데이터 형식

---
+++ 3-5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("%d\n", 123);
+	printf("%5d\n", 123);
+	printf("%05d\n", 123);
+
+	printf("%f\n", 123.45);
+	printf("%7.1f\n", 123.45);
+	printf("%7.3f\n", 123.45);
+
+	printf("%s\n", "Basic-C");
+	printf("%20s\n", "Basic-C");
+}
---
+++ 3-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("\n줄바꿈\n연습\n");
+	printf("\t탭키\t연습 \n");
+	printf("이것을\r덮어씁니다 \n");
+	printf("\a삐소리 1번 \n");
+	printf("글자가 \"강조\"되는 효과 \n");
+	printf("\\\\\\ 역슬래쉬 세개 출력 \n");
+}
