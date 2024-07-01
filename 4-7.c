6f4a7ccfea1fcf9f0b15aeee960cacd28d3deb36
yungun12 <162670150+yungun12@users.noreply.github.com>
2024-07-01 09:11:59 +0900

    C 연산자

---
+++ 4-7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 100, b = -200;
+
+	printf(" 상수의 AND 연산 : %d \n", a && b );
+	printf(" 상수의 OR  연산 : %d \n", a || b  );
+	printf(" 상수의 NOT연산  : %d \n", !a);
+}
