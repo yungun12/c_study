797cb2e7f38d0ed30d089c7c65f857e20dd5dcf9
yungun12 <162670150+yungun12@users.noreply.github.com>
2024-07-08 17:05:48 +0900

    조건문

---
+++ 5-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 200;
+
+	if (a < 100)
+	{
+		printf ("100보다 작군요..\n");
+		printf ("것짓이므로 앞으 문장은 안보이겠죠?\n");
+	}
+
+	printf ("프로그램 끝! \n");
+}
---
+++ 5-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int a = 200;
+
+	if (a <100)
+		printf ("100보다 작군요..\n");
+	else
+		printf ("100보다 크군요..\n");
+}
