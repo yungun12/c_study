4583b7a4c5a87509cd2ac5b2cd04fc387c54ac5a
yungun12 <162670150+yungun12@users.noreply.github.com>
2024-07-15 14:52:18 +0900

    조건문

---
+++ 5-6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+	int a;
+
+	printf("정수를 입력하세요 : ");
+	scanf("%d", &a);
+
+	if ( a%2 == 0)
+	{
+
+		printf ("짝수를 입력했군요..\n");
+	}
+	else
+	{
+		printf ("홀수를 입력했군요..\n");
+	}
+}
